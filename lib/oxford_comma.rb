40063d60010e3bb7e1b5c662701563bff70eb828
Miriam Castillo <castillo.m.mc@gmail.com>
2022-11-16 19:31:15 -0700

    commit tree cba96b6f08d9fc5ae83c835dbdbeb48dafd53ba8

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+    if array.length == 2
+    return "#{array[0]} and #{array[1]}"  
+    elsif 2 < array.length  
+        array[-1].insert(0, "and ")
+      array.join(", ")
+    else
+        array.length ==1
+        return array.join
+    end
+    end  
